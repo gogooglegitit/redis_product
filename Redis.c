bae5aabe4bb2744d7357b7f4ab5222fd53865fda
gogooglegitit <84656967+gogooglegitit@users.noreply.github.com>
2023-03-05 17:43:32 +0000

    Update Redis.c

--- Redis.c
+++ Redis.c
@@ -1,7 +1,10 @@
 
 
+
+
 #include <stdlib.h>
 #include "hiredis/hiredis.h"
+#include <string.h>
 
 
 // welcome you all. this the the program for communication with a redis server
@@ -47,37 +50,91 @@ typedef struct supplier{
 } supplier;
 
 typedef struct productSupplier{
-    product prod;
-    supplier supl;
+    product *prod;
+    supplier *supl;
 } productSupplier;
 
 typedef struct order{
     int order_id;
     int product_id;
     int quantity;
-}
+} order;
 
 
 // add a product into redis, the product code is the key for the hash.
-void add_product_to_redis(redisContext c, redisReply reply, product prod){
+void add_product_to_redis(redisContext *c, redisReply *reply, product *prod){
 
-    reply = redisCommand(c,"HMSET", prod.product_code, "id", prod.product_id, "quantity", prod.quatity, "price", prod.price );
+    reply = redisCommand(c,"HMSET %s %s %d %s %d %s %d", prod->product_code, "id", prod->product_id, "quantity", prod->quatity, "price", prod->price );
 
 }
 
-void add_supplier_to_redis(redisContext c, redisReply reply, supplier supl){
+void add_supplier_to_redis(redisContext *c, redisReply *reply, supplier *supl){
 
-    reply = redisCommand(c,"HMSET", supl.name, "id", supl.supplier_id, "phone", supl.phone);
+    reply = redisCommand(c,"HMSET %s %s %d %s %d", supl->name, "id", supl->supplier_id, "phone", supl->phone);
 }
 
 // assosiats a product to a supplier, a supplier can have mutiple products
-void add_prodSupl_to_redis(redisContext c, redisReply reply, productSupplier ps){
+void add_prodSupl_to_redis(redisContext *c, redisReply *reply, productSupplier *ps, char * number){
     
-    reply = redisCommand(c,"HSET", ps.supl.supplier_id,   )
+    // char *number_to_add;
+
+    // //get the number of fields in this key;
+    // reply = redisCommand(c, "HLEN %d", ps->supl->supplier_id );
+    // printf("passed on read \n");
+    // printf("%d \n", reply->type);
+    // printf("%d \n", reply->integer);
+
+    // if (reply == NULL || reply->type == REDIS_REPLY_NIL ){
+    //     number_to_add = "0";
+    // }
+    // else{
+    //     printf("in else \n");
+    //     number_to_add =  reply->integer;
+    // }
+    // printf("passed on assign to add_to \n");
+
+    // printf("%d\n", number_to_add);
+
+    // char prod_number[20] = "product n.";
+
+    // strcat(prod_number, number_to_add);
+
+    // printf("passed strcat \n");
+
+    reply = redisCommand(c,"HMSET %d %s %s", ps->supl->supplier_id, number, ps->prod->product_code );
 }
 
 
 
+product *make_product(int pro_id, char *pro_code, int qua, int price ){
+    product *pro = malloc(sizeof(product));
+    pro->product_id = pro_id;
+    pro->product_code = pro_code;
+    pro->quatity = qua;
+    pro->price = price;
+
+    return pro;
+    }
+supplier *make_supplier( char *name, int supl_id, int phone){
+    supplier *sup = malloc(sizeof(supplier));
+    sup->name = name;
+    sup->supplier_id = supl_id;
+    sup->phone = phone;
+
+    return sup;
+}
+productSupplier *make_productSuplier(product *pro,supplier *sup ){
+    productSupplier *proSup = malloc(sizeof(productSupplier));
+    proSup->prod = pro;
+    proSup->supl = sup;
+
+    return proSup;
+}
+
+
+void destory_product( product *pro){
+    free(pro);
+}
 
 
 int main(int argc, char **argv[]){
@@ -95,6 +152,31 @@ if (c != NULL && c->err) {
 
 redisReply *reply;
 reply = redisCommand(c, "SET %s %s", "foo", "bar");
+reply = redisCommand(c, "HMSET %s %s %d", "isGlass", "pruduct id", 12343);
+
+product *pro1 = make_product(1234,"glass",43,2000);
+add_product_to_redis(c, reply, pro1);
+
+product *pro2 = make_product(432,"bok",20034,70200);
+add_product_to_redis(c, reply, pro2);
+
+product *pro3 = make_product(543312,"leksak",9900,3);
+add_product_to_redis(c, reply, pro3);
+
+
+supplier *sup1 = make_supplier("IKEA",123,0715160202);
+add_supplier_to_redis(c,reply, sup1);
+
+supplier *sup2 = make_supplier("GOGO",12345 ,5432);
+add_supplier_to_redis(c,reply, sup2);
+
+productSupplier *ikea1 = make_productSuplier(pro2,sup1);
+productSupplier *ikea2 = make_productSuplier(pro3,sup1);
+productSupplier *GOGO = make_productSuplier(pro1,sup2);
+
+add_prodSupl_to_redis(c,reply, ikea1,"1");
+add_prodSupl_to_redis(c,reply, ikea2,"2");
+add_prodSupl_to_redis(c,reply, GOGO,"1");
 
 freeReplyObject(reply);
 
@@ -102,3 +184,4 @@ freeReplyObject(reply);
 
 redisFree(c);
 }
+
